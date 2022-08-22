f25e1a895ca8e78d05b699db379f5dae67f12882
Michael Nam <104886051+namcvs@users.noreply.github.com>
2022-08-22 22:55:15 +0700

    ProjectWeb

---
+++ support.js
@@ -0,0 +1,7 @@
+
+function click_menu(){
+const menu= document.querySelector('.dropdown-menu');
+menu.classList.toggle('active_menu');
+}
+// classList trả về các tên lớp CSS của một phần tử.
+// .toggle(): Hiển thị và ẩn các thành phần phù hợp.
